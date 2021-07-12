64d8522a0f057e14fa99f49d68197e42501a4cc1
“lian.yanmin” <lian.yanmin@immomo.com>
2021-07-12 16:05:18 +0800

    提交

--- 666/666/ViewController.m
+++ 666/666/ViewController.m
@@ -31,6 +31,11 @@
 
 }
 
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    
+    NSLog(@"====哈哈哈哈");
+    
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
